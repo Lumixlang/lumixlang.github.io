44aa28c0e5b29b26b0f14d99efd86e773d555694
MathisSparky <145775791+MathisSparky@users.noreply.github.com>
2023-10-07 22:23:15 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function installLumix() {
+    alert('Lumix installed successfully!');
+}
+function copyCode() {
+    navigator.clipboard.writeText(`op main() {
+greeting = "Hello, world!";
+print(greeting);
+}`)
+}
