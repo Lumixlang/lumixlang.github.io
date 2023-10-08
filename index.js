40a71b71920d682f0a6c541538715f89dbcfd8c6
MathisSparky <145775791+MathisSparky@users.noreply.github.com>
2023-10-08 08:44:31 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function installLumix() {
-    alert('Lumix installed successfully!');
-}
-function copyCode() {
-    navigator.clipboard.writeText(`op main() {
-greeting = "Hello, world!";
-print(greeting);
-}`)
-}
