b20ad86247c8c05d8160c9e12034d11820e51a40
MathisSparky <145775791+MathisSparky@users.noreply.github.com>
2023-11-16 10:08:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function installLumix() {
     alert("In maintenance.");
 }
 function copyCode() {
-    navigator.clipboard.writeText(`op main() {
-greeting = "Hello, world!";
-print(greeting);
+    navigator.clipboard.writeText(`main() {
+    greeting = "Hello, world!";
+    print(greeting);
 }`)
     alert("Copied to clipboard.");
 }
