6f1c8b144dc51bf42a2848328ac11033109b5125
Sparky <145775791+MathisSparky@users.noreply.github.com>
2023-12-04 15:21:16 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 function installLumix() {
     alert("In maintenance.");
 }
-function copyCode() {
-    navigator.clipboard.writeText(`main() {
-    greeting = "Hello, world!";
-    print(greeting);
-}`)
-    alert("Copied to clipboard.");
+
+var currentImageIndex = 0;
+var galleryImages = document.querySelectorAll('.gallery-image');
+var prevBtn = document.getElementById('prev-btn');
+var textContainers = ["Pfirst", "Psecond"];
+
+function showImage(index) {
+    galleryImages.forEach(function (image, i) {
+        if (i === index) {
+            image.style.display = 'block';
+        } else {
+            image.style.display = 'none';
+        }
+    });
+
+    textContainers.forEach(function (textContainer, i) {
+        var text = document.getElementById(textContainer);
+        if (text) {
+            text.style.display = i === index ? 'block' : 'none';
+        }
+    });
+
+    prevBtn.disabled = index === 0;
 }
+
+function changeImage(offset) {
+    currentImageIndex += offset;
+
+    if (currentImageIndex < 0) {
+        currentImageIndex = 0;
+    } else if (currentImageIndex >= galleryImages.length) {
+        currentImageIndex = galleryImages.length - 1;
+    }
+
+    showImage(currentImageIndex);
+}
+
+showImage(currentImageIndex);
